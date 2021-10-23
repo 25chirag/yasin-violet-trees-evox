e5a726dfb3d481c032edc9295a2795834c27ff66
25chirag <91938054+25chirag@users.noreply.github.com>
2021-10-23 18:25:34 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,14 @@
 #
 # Copyright (C) 2018-2020 The LineageOS Project
-# Copyright (C) 2021 The EvolutionX Project
+# Copyright (C) 2021 The SSOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_violet.mk
+    $(LOCAL_DIR)/ssos_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_violet-userdebug \
-    evolution_violet-user \
-    evolution_violet-eng
+    ssos_violet-userdebug \
+    ssos_violet-user \
+    ssos_violet-eng
